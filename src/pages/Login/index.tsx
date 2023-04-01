ef84b44d468c052ec6f11669f052dee4d16fd27b
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-04-01 08:42:40 +0100

    login form setup

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,19 @@
 import style from "./index.module.scss";
-import loginPageImage from "../../assets/login-page-image.png"
+import loginPageImage from "../../assets/login-page-image.png";
+
 const Login = () => {
   return (
     <section>
       <div>
-        <img
-          src={loginPageImage}
-          alt="Login Main image"
-          role="presentation"
-        />
+        <img src={loginPageImage} alt="Login Main image" role="presentation" />
+      </div>
+      <div>
+        <h2>Welcome!</h2>
+        <p>Enter details to login.</p>
+        <form>
+          <input placeholder="password" type="email" />
+          <input placeholder="password" type="password" />
+        </form>
       </div>
     </section>
   );
