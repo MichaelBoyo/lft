f74d3b9239a3af53fb0fb0481b32412c0b4dd73d
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-04-01 08:32:18 +0100

    added main login image

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
 import style from "./index.module.scss";
+import loginPageImage from "../../assets/login-page-image.png"
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section>
+      <div>
+        <img
+          src={loginPageImage}
+          alt="Login Main image"
+          role="presentation"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Login;
