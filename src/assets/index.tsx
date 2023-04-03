134bbf9e6d069ebc52c9ed6924d40dac711de16d
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-04-03 17:17:59 +0100

    unified asset exports

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -16,7 +16,53 @@ import Users from "./icons/Users";
 import LeftNavigation from "./icons/LeftNavigation";
 import RightNavigation from "./icons/RightNavigation";
 import NotFound from "./icons/NotFound";
+import OrgDropDown from "./icons/OrgDropDown";
+import Loading from "./icons/Loading";
+import DropDown from "./icons/DropDown";
+import Notification from "./icons/Notification";
+import Logo from "./icons/Logo";
+import Search from "./icons/Search";
+import userImage from "../assets/user.png";
+import FeesAndCharges from "./icons/FeesAndCharges";
+import Home from "./icons/Home";
+import LoanRequests from "./icons/LoanRequests";
+import Karma from "./icons/Karma";
+import Reports from "./icons/Reports";
+import Handshake from "./icons/Handshake";
+import Guarantors from "./icons/Guarantors";
+import FeesAndPricing from "./icons/FeesAndPricing";
+import { SideBarLink, SidebarSection } from "../components";
+import Services from "./icons/Services";
+import ServiceAccount from "./icons/ServiceAccount";
+import Sack from "./icons/Sack";
+import Savings from "./icons/Savings";
+import LogOut from "./icons/LogOut";
+import SavingsProducts from "./icons/SavingsProducts";
+import SystemMessages from "./icons/SystemMessages";
+import Settlements from "./icons/Settlements";
+import Transactions from "./icons/Transactions";
+import WhiteList from "./icons/WhiteList";
+import Preferences from "./icons/Preferences";
+import View from "./icons/View";
+import SideMenu from "./icons/SideMenu";
+import Filter from "./icons/Filter";
 export {
+  Filter,
+  SideMenu,
+  View,
+  Preferences,
+  WhiteList,
+  Transactions,
+  Settlements,
+  SystemMessages,
+  Services,
+  ServiceAccount,
+  SavingsProducts,
+  Savings,
+  Sack,
+  LogOut,
+  SideBarLink,
+  SidebarSection,
   avatar,
   Activate,
   AuditLogs,
@@ -34,4 +80,19 @@ export {
   LeftNavigation,
   RightNavigation,
   NotFound,
+  OrgDropDown,
+  Loading,
+  DropDown,
+  Logo,
+  Notification,
+  Search,
+  userImage,
+  FeesAndCharges,
+  FeesAndPricing,
+  Guarantors,
+  Handshake,
+  Home,
+  Karma,
+  LoanRequests,
+  Reports,
 };
