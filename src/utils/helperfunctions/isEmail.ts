5b3a21c4f9833600607f879bc859961324cf630c
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-04-04 11:33:12 +0100

    refactored isPassword.ts

--- src/utils/helperfunctions/isEmail.ts
+++ src/utils/helperfunctions/isEmail.ts
@@ -1,6 +1,5 @@
 const isEmail = (email: string): boolean => {
   const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-  const isValid = regex.test(email);
-  return isValid;
+  return regex.test(email);
 };
 export default isEmail;
